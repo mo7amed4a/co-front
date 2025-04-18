1bfbc6e1e9dd534f545ac799befb447d7cfd29dd
mo7amed4a <m7md0a99@gmail.com>
2025-04-18 10:32:31 +0200

    error

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -29,9 +29,9 @@ export default async function page() {
   });
   const data = res.data.data.ratings;
   const hero = res.data.data.feedbackPage.hero;
-  return (
+  return  data &&  (
     <div>
-      <HeroFeedback data={hero} />
+      {hero && <HeroFeedback data={hero} />}
       <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-50">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
